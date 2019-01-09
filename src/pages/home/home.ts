2b816e1dd100f6032b886f1a07d0f1775cd73523
Gleicy Maria <gleicy.maria@hotmail.com>
2019-01-09 15:08:42 -0300

    Criando a tela de login e a home, com botão de login e logout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+   
+  public username:string ="Gleicy";
+  
   constructor(public navCtrl: NavController) {
 
+  }
+
+  logout(){
+    this.navCtrl.setRoot(LoginPage);
   }
 
 }
