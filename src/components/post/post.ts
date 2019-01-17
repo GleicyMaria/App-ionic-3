76248ca4fbfbb815426346099f7b54706427230a
Gleicy Maria <gleicy.maria@hotmail.com>
2019-01-17 15:59:10 -0300

    limite de caracteres, botão desabilitado e barra de status

--- src/components/post/post.ts
+++ src/components/post/post.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DetalhePostPage } from '../../pages/detalhe-post/detalhe-post';
 
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,32 +3,61 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  
+  dados={
+    username:'',
+    password:''
+  };
 
- 
+  botaoaDesabilitado = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  habilitar:boolean = false;
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+     ) {
+     
+     
+  
   }
   
-  login(){
+  login(form){
     console.log("login");
+    console.log(form);
+   
     this.navCtrl.setRoot(HomePage);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
+ 
+  onChangeTime(username,password){
+    if(this.lenghtUser(username)  && this.lenght(password)){
+      console.log("aqui")
+    }
+
+  }
+ 
+  lenghtUser(username) {
+    if(username.lenght >=3 ){
+      return true;
+    }else{
+      return false
+    }
+  } 
+
+  lenght(password) {
+    if(password.lenght >=4 ){
+      return true;
+    }else{
+      return false
+    }
+  } 
 
 }
