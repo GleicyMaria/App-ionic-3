9e68047a8a081c2ba63447fcc90deb71dfc6ec74
Gleicy Maria <gleicy.maria@hotmail.com>
2019-01-18 09:33:11 -0300

    correção de erros

--- src/pages/lista-post/lista-post.ts
+++ src/pages/lista-post/lista-post.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { PostProvider } from '../../providers/post/post';
+
 
 /**
  * Generated class for the ListaPostPage page.
@@ -21,21 +21,13 @@ export class ListaPostPage {
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
-     private postPrivider:PostProvider) {
+     ) {
        
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListaPostPage');
-    this.postPrivider.getPosts().subscribe(
-      (data) =>{
-        console.log(data)
-        this.listPosts = data;
-       
-      }, error =>{
-        console.log(error);
-      }
-    )
+    
   }
 
   
