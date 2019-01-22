98fba7c1f7c164f689ef5773fb06966c504557f5
Gleicy Maria <gleicy.maria@hotmail.com>
2019-01-22 16:54:25 -0300

    Usando a camera

--- src/pages/alterar-foto/alterar-foto.ts
+++ src/pages/alterar-foto/alterar-foto.ts
@@ -28,7 +28,7 @@ export class AlterarFotoPage {
     
    const options:CameraOptions ={
       quality:100,
-      destinationType:this.camera.DestinationType.NATIVE_URI,
+      destinationType:this.camera.DestinationType.DATA_URL,
       encodingType:this.camera.EncodingType.JPEG,
       mediaType:this.camera.MediaType.PICTURE,
       sourceType: type == "picture" ? 
